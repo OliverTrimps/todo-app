69ff0978b8526519940e9b268607168fa0a1cdb1
Selere Oliver <selereademola@gmail.com>
2023-03-12 20:07:54 +0100

    initial commit

--- forms.py
+++ forms.py
@@ -1,13 +1,13 @@
 import datetime
 
 from flask_wtf import FlaskForm
-from wtforms import BooleanField, TextAreaField, SubmitField, StringField, DateField, SelectField, HiddenField, DateTimeField
+from wtforms import BooleanField, TextAreaField, SubmitField, StringField, DateField, SelectField, HiddenField
 from wtforms.validators import DataRequired, InputRequired
 from datetime import datetime
 
 # Successfully installed WTForms-3.0.1 flask-wtf-1.1.1
 
-day = datetime.today()
+day = datetime.today().date()
 # today = day.strftime("%Y-%m/%d")
 
 
@@ -20,7 +20,7 @@ class UserForm(FlaskForm):
 class CreateTodoForm(FlaskForm):
     todo = TextAreaField(label='What to do?', validators=[DataRequired()])
     # due_date = DateField(label="Due Date", validators=[InputRequired(), DataRequired()], default=today)
-    due_date = DateTimeField(label="Due Date", format="%Y-%m-%dT%H:%M:%S", default=day, validators=[InputRequired(), DataRequired()])
+    due_date = StringField(label="Due Date", default=day, validators=[InputRequired(), DataRequired()])
     # completed = BooleanField(label='Completed?', default=False, render_kw={'checked': ''})
     completed = SelectField(label='Todo Status', choices=['Done', 'Pending'], default='Pending')
     submit = SubmitField(label='Finish')
